bb3f26b1a8173cee8de0107d863bb53d847374be
Katerina Nefedova <Nefedova.Katia@yandex.ru>
2023-08-10 12:22:21 +0400

    корректировка тестов

--- src/test/java/com/github/Katerina163/algorithm/BubbleSortTest.java
+++ src/test/java/com/github/Katerina163/algorithm/BubbleSortTest.java
@@ -6,33 +6,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class BubbleSortTest {
+    private BubbleSort sort = new BubbleSort();
+
     @Test
     public void correct() {
         int[] test = {1, 2, 3, 0, 6, 5, 4};
-        var result = BubbleSort.sort(test);
+        sort.sort(test);
         int[] waiting = {0, 1, 2, 3, 4, 5, 6};
-        assertThat(result, is(waiting));
+        assertThat(test, is(waiting));
     }
 
     @Test
     public void zero() {
         int[] test = {0};
-        var result = BubbleSort.sort(test);
-        assertThat(result, is(test));
+        sort.sort(test);
+        int[] waiting = {0};
+        assertThat(test, is(waiting));
     }
 
     @Test
     public void correctOpt() {
         int[] test = {1, 2, 3, 0, 6, 5, 4};
-        var result = BubbleSort.sortOptimization(test);
+        sort.sortOptimization(test);
         int[] waiting = {0, 1, 2, 3, 4, 5, 6};
-        assertThat(result, is(waiting));
+        assertThat(test, is(waiting));
     }
 
     @Test
     public void zeroOpt() {
         int[] test = {0};
-        var result = BubbleSort.sortOptimization(test);
-        assertThat(result, is(test));
+        sort.sortOptimization(test);
+        int[] waiting = {0};
+        assertThat(test, is(waiting));
     }
 }
--- src/test/java/com/github/Katerina163/algorithm/QuickSortTest.java
+++ src/test/java/com/github/Katerina163/algorithm/QuickSortTest.java
@@ -6,10 +6,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class QuickSortTest {
+    private QuickSort sort = new QuickSort();
+
     @Test
     public void correct() {
         int[] test = {1, 2, 3, 0, 6, 5, 4};
-        QuickSort.sort(test, 0, test.length - 1);
+        sort.sort(test, 0, test.length - 1);
         int[] waiting = {0, 1, 2, 3, 4, 5, 6};
         assertThat(test, is(waiting));
     }
@@ -17,7 +19,23 @@ class QuickSortTest {
     @Test
     public void zero() {
         int[] test = {0};
-        QuickSort.sort(test, 0, test.length - 1);
+        sort.sort(test, 0, 0);
+        int[] res = {0};
+        assertThat(test, is(res));
+    }
+
+    @Test
+    public void correctOverride() {
+        int[] test = {1, 2, 3, 0, 6, 5, 4};
+        sort.sort(test);
+        int[] waiting = {0, 1, 2, 3, 4, 5, 6};
+        assertThat(test, is(waiting));
+    }
+
+    @Test
+    public void zeroOverride() {
+        int[] test = {0};
+        sort.sort(test);
         int[] res = {0};
         assertThat(test, is(res));
     }
